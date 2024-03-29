31f40354db17ddc633acd4b50a131546bfc724c6
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-03-29 13:41:45 +0800

    I haven't added any code within the LandinScreen, that's why.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-
-{
-  /* Screens*/
-}
 import LandingScreen from "./screens/LandingScreen";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="LandingScreen"
-        component={LandingScreen}
-        options={{ headerShown: false, statusBarHidden: true }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LandingScreen">
+        <Stack.Screen
+          name="LandingScreen"
+          component={LandingScreen}
+          options={{ headerShown: false, statusBarHidden: true }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/LandingScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LandingScreen = () => {
+  return (
+    <View>
+      <Text>LandingScreen</Text>
+    </View>
+  )
+}
+
+export default LandingScreen
+
+const styles = StyleSheet.create({
+    
+})
