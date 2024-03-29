aa57268f4ff642187c24d383388f066d69df0a04
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-03-29 13:50:22 +0800

    Modified LandingScreen

--- screens/LandingScreen.tsx
+++ screens/LandingScreen.tsx
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { logo } from "../loadSVG";
+import { SvgXml } from "react-native-svg";
 
 const LandingScreen = () => {
-  return (
-    <View>
-      <Text>LandingScreen</Text>
-    </View>
-  )
-}
+    return (
+      <View style={styles.mainContainer}>
+        <SvgXml xml={logo}  />
+      </View>
+    );
+};
 
-export default LandingScreen
+export default LandingScreen;
 
 const styles = StyleSheet.create({
-    
-})
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+});
