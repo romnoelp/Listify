a5f02e5826bd9480749890d44fd4bddfb25e9427
Richmond Baltazar <baltazarrichmond31@gmail.com>
2024-03-29 14:41:48 +0800

    configuration setup

--- App.tsx
+++ App.tsx
@@ -1,33 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LandingScreen from "./screens/LandingScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import LoginScreen from "./screens/LoginScreen";
 
-const Stack = createNativeStackNavigator();
+const MainStack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LandingScreen">
-        <Stack.Screen
-          name="LandingScreen"
-          component={LandingScreen}
-          options={{ headerShown: false, statusBarHidden: true }}
-        />
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={{ headerShown: false, statusBarHidden: true }}
-        />
-        <Stack.Screen
-          name="RegisterScreen"
-          component={RegisterScreen}
-          options={{ headerShown: false, statusBarHidden: true }}
-        />
-      </Stack.Navigator>
+      <MainStack.Navigator>
+        <MainStack.Screen name="LandingScreen" component={LandingScreen} />
+      </MainStack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
