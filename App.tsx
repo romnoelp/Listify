fbf507515abf0a0d01441ad2edfcda38bad2dc0f
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-03-29 13:26:06 +0800

    Trying to initialize some shizz.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+{
+  /* Screens*/
 }
+import LandingScreen from "./screens/LandingScreen";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="LandingScreen"
+        component={LandingScreen}
+        options={{ headerShown: false, statusBarHidden: true }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>;
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
