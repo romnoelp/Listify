475bd26db0a49864a9eb4d41dec10d87025dcf99
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-04-01 23:03:00 +0800

    Placed the button on all screens

--- screens/OngoingScreen.tsx
+++ screens/OngoingScreen.tsx
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
-import { Entypo } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { Entypo } from "@expo/vector-icons";
+import AddModal from "../components/AddModal";
+import { useTaskContext } from "../context/toDoTaskContext";
+import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import { auth, db } from "../firebaseConfig";
+import { ToDoTask } from "../types";
+import Toast from "react-native-simple-toast";
+import FloatingButton from "../components/FloatingButton";
 
 interface Task {
   id: number;
@@ -12,17 +29,103 @@ interface Task {
 const OngoingScreen = () => {
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: "Complete homework", status: "On Going" },
-    { id: 4, title: "Bebetaym", status: "On Going"}
+    { id: 4, title: "Bebetaym", status: "On Going" },
   ]);
+
+  const [isAddTaskModalVisible, setIsAddTaskModalVisible] = useState(false);
+  const [dueDate, setDueDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [showClock, setShowClock] = useState(false);
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const { addTask, TasksList } = useTaskContext();
+  const user = auth.currentUser;
 
   const handleToggleTaskStatus = (id: number) => {
     // Implement logic to change task status
-  }
+  };
 
   const handleDeleteTask = (id: number) => {
     // Implement logic to delete task
-  }
-  
+  };
+
+  const formatDateString = (date: Date): string => {
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  const onChangeTime = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    if (event.type === "set" && selectedDate) {
+      const currentDate = selectedDate;
+      setDueDate(currentDate);
+      if (Platform.OS === "android") {
+        setShowClock(false);
+      }
+    }
+  };
+
+  const onChangeDate = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    if (event.type === "set" && selectedDate) {
+      const currentDate = selectedDate;
+      setDueDate(currentDate);
+      if (Platform.OS === "android") {
+        setShowClock(true);
+        setShowCalendar(false);
+      }
+    }
+    showDatepicker(); // Always call showDatepicker after handling date change
+  };
+
+  const showDatepicker = () => {
+    setShowCalendar(!showCalendar);
+  };
+
+  const saveTask = async () => {
+    try {
+      if (user) {
+        const docRef = db
+          .collection("users")
+          .doc(user.displayName?.toString())
+          .collection("Tasks");
+
+        const CurrentDate = new Date();
+        console.log(CurrentDate);
+        const statusCheck = CurrentDate > dueDate ? "OverDue" : "OnGoing";
+        await docRef.add({
+          taskTitle,
+          taskDescription,
+          dueDate: dueDate,
+          status: statusCheck,
+        });
+
+        const newTask: ToDoTask = {
+          id: docRef.id,
+          taskTitle,
+          taskDescription,
+          dueDate,
+          status: statusCheck,
+        };
+
+        addTask(newTask);
+        Toast.show("Added Successfully", Toast.SHORT);
+        closeAddTaskModal();
+      }
+    } catch (error) {
+      Toast.show("Error occured, try again later", Toast.SHORT);
+    }
+  };
+
+  const closeAddTaskModal = () => {
+    setIsAddTaskModalVisible(!isAddTaskModalVisible);
+    setTaskDescription("");
+    setTaskTitle("");
+  };
+
   const renderItem = ({ item }: { item: Task }) => (
     <View style={styles.taskContainer}>
       <TouchableOpacity onPress={() => handleToggleTaskStatus(item.id)}>
@@ -35,7 +138,6 @@ const OngoingScreen = () => {
         )}
       </TouchableOpacity>
       <Text style={styles.taskText}>{item.title}</Text>
-
     </View>
   );
 
@@ -43,10 +145,47 @@ const OngoingScreen = () => {
     <View style={styles.container}>
       <Text style={styles.header}>On Going</Text>
       <FlatList
-        data={tasks.filter(task => task.status === "On Going")}
+        data={tasks.filter((task) => task.status === "On Going")}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={styles.listContainer}
+      />
+      <View
+        style={{
+          position: "absolute",
+          bottom: wp(10),
+          right: wp(40),
+          left: wp(40),
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/*Change to the floating button rotation shit  */}
+        <FloatingButton
+          onAddItemsPress={() => setIsAddTaskModalVisible(true)}
+          onDeleteAllItemsPress={function (): void {
+            throw new Error(
+              "Where's the function, cuh? Define it first, bish."
+            );
+          }}
+        ></FloatingButton>
+      </View>
+      <AddModal //use this to show addModal
+        dueDate={dueDate}
+        formatDateString={formatDateString}
+        isAddTaskModalVisible={isAddTaskModalVisible}
+        onChangeDate={onChangeDate}
+        onChangeTime={onChangeTime}
+        setIsAddTaskModalVisible={setIsAddTaskModalVisible}
+        setShowCalendar={setShowCalendar}
+        showCalendar={showCalendar}
+        showClock={showClock}
+        saveTask={saveTask}
+        setTaskTitle={setTaskTitle}
+        setTaskDescription={setTaskDescription}
+        taskDescription={taskDescription}
+        taskTitle={taskTitle}
+        closeAddTaskModal={closeAddTaskModal}
       />
     </View>
   );
@@ -96,8 +235,6 @@ const styles = StyleSheet.create({
     fontSize: wp(4),
     flex: 1,
   },
-
-  
 });
 
 export default OngoingScreen;
